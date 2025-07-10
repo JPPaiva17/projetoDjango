5280709d4d335976ba4c690b0198fd58ec0ec4e3
Joao Pedro Paiva <jpppaiva1706@gmail.com>
2025-07-09 23:24:07 -0300

    Criação do app receitas

--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projeto project.
+URL configuration for project1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.http import HttpResponse
+
+
 from django.contrib import admin
 from django.urls import path
 
+def my_view(request):
+    return HttpResponse('João Pedro Paiva')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
